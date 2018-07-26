0eff635cdf2e3d1108c1ccac0eb5c3e9e4957983
Keno <keno.0612.judo@icloud.com>
2018-07-26 22:49:56 +0900

    add patch

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -1,27 +1,14 @@
 const {ObjectID} = require('mongodb');
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
-
-var id = '5b548588e5db165209a45159';
-
-if (!ObjectID.isValid(id)) {
-  console.log('ID not valid');
-}
-// Todo.find({
-//   _id: id
-// }).then((todo) => {
-//   console.log('todos', todo);
-// });
-//
-// Todo.findOne({
-//   _id: id
-// }).then((todo) => {
-//   console.log('todo', todo);
+//delete all
+// Todo.remove({}).then((result) => {
+//   console.log(result);
 // });
 
-Todo.findById(id).then((todo) => {
-  if (!todo) {
-    return console.log('Id not found');
-  }
-  console.log('todo by id', todo);
-}).catch((e)=> console.log(e));
+//delete first
+//Todo.findOneAndRemove]
+
+Todo.findByIdAndRemove('id').then((result) => {
+  console.log(result);
+});
--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,13 @@
 var mongoose = require('mongoose');
+
+
 var User = mongoose.model('User', {
   email: {
     type: String,
     required: true, // validation
     minlength: 1,
-    trim: true
+    trim: true,
+    unique: true
   }
 });
 
